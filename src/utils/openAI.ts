2c65466257c7c82f6784dffbe5c194d7bedcdb02
JerryYao80 <98963023+JerryYao80@users.noreply.github.com>
2024-12-26 22:51:33 +0800

    修改为gemini 2.0

--- src/utils/openAI.ts
+++ src/utils/openAI.ts
@@ -8,7 +8,7 @@ const genAI = apiBaseUrl
   : new GoogleGenerativeAI(apiKey)
 
 export const startChatAndSendMessageStream = async(history: ChatMessage[], newMessage: string) => {
-  const model = genAI.getGenerativeModel({ model: 'gemini-pro' })
+  const model = genAI.getGenerativeModel({ model: 'gemini-2.0-flash-exp' }) // gemini-pro
 
   const chat = model.startChat({
     history: history.map(msg => ({
@@ -19,11 +19,11 @@ export const startChatAndSendMessageStream = async(history: ChatMessage[], newMe
       maxOutputTokens: 8000,
     },
     safetySettings: [
-      {category: 'HARM_CATEGORY_HARASSMENT', threshold: 'BLOCK_NONE'},
-      {category: 'HARM_CATEGORY_HATE_SPEECH', threshold: 'BLOCK_NONE'},
-      {category: 'HARM_CATEGORY_SEXUALLY_EXPLICIT', threshold: 'BLOCK_NONE'},
-      {category: 'HARM_CATEGORY_DANGEROUS_CONTENT', threshold: 'BLOCK_NONE'}
-      ],
+      { category: 'HARM_CATEGORY_HARASSMENT', threshold: 'BLOCK_NONE' },
+      { category: 'HARM_CATEGORY_HATE_SPEECH', threshold: 'BLOCK_NONE' },
+      { category: 'HARM_CATEGORY_SEXUALLY_EXPLICIT', threshold: 'BLOCK_NONE' },
+      { category: 'HARM_CATEGORY_DANGEROUS_CONTENT', threshold: 'BLOCK_NONE' },
+    ],
   })
 
   // Use sendMessageStream for streaming responses
